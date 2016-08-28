bb0b4367b9ac576bedae2691343b4d885b392567
Raul Leite <raul.abreu.leite@gmail.com>
2016-08-27 21:00:51 -0300

    Done

---
+++ promise-shop/06_throw_an_error.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const json = process.argv[2];
+
+function parsePromised() {
+    return new Promise(function(fulfill, reject) {
+        try {
+            fulfill(JSON.parse(json));
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
+parsePromised().then(null, console.log)
