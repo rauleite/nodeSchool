3e022894ffbe82bbe03226a7e6558a80d1887c9b
Raul Leite <raul.abreu.leite@gmail.com>
2016-08-29 12:38:23 -0300

    done

---
+++ learn-generators/06-look-sync-do-async.js
@@ -0,0 +1,26 @@
+'use strict'
+
+let fs = require('fs')
+
+function run (generator) {
+  let it = generator(go)
+
+  function go (err, result) {
+    if (err) return it.throw(err)
+    it.next(result)
+  }
+
+  go()
+}
+
+run(function * (done) {
+  let firstFile
+  try {
+    let dirFiles = yield fs.readdir('NoNoNoNo', done) // No such dir
+    firstFile = dirFiles[0]
+  } catch (err) {
+    firstFile = null
+  }
+
+  console.log(firstFile)
+})
